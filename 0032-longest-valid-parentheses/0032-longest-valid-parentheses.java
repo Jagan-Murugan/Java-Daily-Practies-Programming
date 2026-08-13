aa36c45ff800267da4b6970e0e24d9a5de29108a
Jagan-Murugan <jagankbd5@gmail.com>
2026-08-13 20:43:39 +0530

    Time: 2 ms (97.03%), Space: 44.6 MB (92.7%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int left = 0,right = 0,max = 0;
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '('){
+                left++;
+            }
+            else
+            right++;
+            if(left == right)
+            max = Math.max(max,2*right);
+        if(right>left){
+            left = 0;
+            right = 0;
+        }
+        }
+        left = 0;
+        right = 0;
+        for(int i = s.length()-1;i>0;i--){
+            if(s.charAt(i) == '('){
+               left++;
+            }
+            else{
+                right++; 
+            }
+            if(left == right){
+                max = Math.max(max,2*right);
+            }
+            if(left>right){
+                left = 0;
+                right = 0;
+            }
+        }
+        return max;
+        
+    }
+}
