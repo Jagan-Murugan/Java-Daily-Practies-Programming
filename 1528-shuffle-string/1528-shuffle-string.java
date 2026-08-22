5cc543896900329329099ef055095bcaec235140
Jagan-Murugan <jagankbd5@gmail.com>
2026-08-22 21:02:54 +0530

    Time: 0 ms (100%), Space: 45.2 MB (96.73%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -1,15 +1,20 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
         
-        char[] res = new char[indices.length];
-        for(int i = 0;i<s.length();i++){
-            res[indices[i]] = s.charAt(i);
+        // char[] res = new char[indices.length];
+        // for(int i = 0;i<s.length();i++){
+        //     res[indices[i]] = s.charAt(i);
+        // }
+        // String temp = "";
+        // for(int j = 0;j < res.length;j++){
+        //     temp = temp+res[j];
+        // }
+        // return temp;
+
+        char[] ans = new char[s.length()];
+        for(int i = 0;i < indices.length;i++){
+            ans[indices[i]] = s.charAt(i); 
         }
-        String temp = "";
-        for(int j = 0;j < res.length;j++){
-            temp = temp+res[j];
-        }
-        return temp;
-        
+        return new String(ans);
     }
 }
