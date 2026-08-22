87ef8196295c6b8f8bcaeb843da24b8db2311be4
Jagan-Murugan <jagankbd5@gmail.com>
2026-08-22 20:59:00 +0530

    Time: 5 ms (5.58%), Space: 46.3 MB (7.37%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        
+        char[] res = new char[indices.length];
+        for(int i = 0;i<s.length();i++){
+            res[indices[i]] = s.charAt(i);
+        }
+        String temp = "";
+        for(int j = 0;j < res.length;j++){
+            temp = temp+res[j];
+        }
+        return temp;
+        
+    }
+}
