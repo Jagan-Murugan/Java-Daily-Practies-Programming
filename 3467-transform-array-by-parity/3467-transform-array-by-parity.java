5ebe6e22b16c3e561ef8dd3f8a5145bff7807312
Jagan-Murugan <jagankbd5@gmail.com>
2026-08-22 21:17:38 +0530

    Time: 1 ms (99.75%), Space: 46.7 MB (73.11%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-        int[] res = new int[nums.length];
+       /* int[] res = new int[nums.length];
         for(int i = 0;i<nums.length;i++){
             if(nums[i] % 2 == 0){
                 res[i] = 0;
@@ -10,6 +10,17 @@ class Solution {
             }
         }
         Arrays.sort(res);
-        return res;
+        return res;*/
+        int even = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] % 2 == 0){
+                even++;
+            }
+        }
+        int[] ans = new int[nums.length];
+        for(int i = even;i<nums.length;i++){
+            ans[i] = 1;
+        } 
+        return ans;
     }
 }
