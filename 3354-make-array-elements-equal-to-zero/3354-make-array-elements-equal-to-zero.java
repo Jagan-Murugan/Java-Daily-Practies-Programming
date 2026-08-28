01cb08e37becbd807fd489432491621846af0b7f
Jagan-Murugan <jagankbd5@gmail.com>
2026-08-28 21:54:44 +0530

    Time: 100 ms (17.16%), Space: 45 MB (14.79%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-       int totalsum = 0;
+      /* int totalsum = 0;
        int n = nums.length;
        for(int num : nums){
         totalsum += num;
@@ -20,5 +20,56 @@ class Solution {
         leftsum += nums[i];
        }
        return count;
+       }
+       }*/
+
+    //    NORMAL METHOD 
+    int count = 0;
+        // Check every position
+        for (int i = 0; i < nums.length; i++) {
+
+            // We can start only from 0
+            if(nums[i]==0){
+                // Try left direction
+                if(valid(nums,i,-1)){
+                    count++;
+                }
+                // Try right direction
+                if(valid(nums,i,1)){
+                    count++;
+                }
+        }
+        }
+        return count;
     }
+    public static boolean valid(int[] nums,int curr,int direction){
+        int[] copy = nums.clone();
+
+        while(curr >= 0 && curr < copy.length){
+          // If current element is 0
+            if (copy[curr] == 0) {
+
+                // Move in the current direction
+                curr = curr + direction;
+
+            } else {
+
+                // Decrement nums[curr] by 1
+                copy[curr]--;
+
+                // Reverse direction
+                direction = -direction;
+
+                // Take one step in the new direction
+                curr = curr + direction;
+            }
+        }
+        for(int i = 0;i<copy.length;i++){
+            if(copy[i]!=0){
+              return false;
+            }
+        }
+        return true;
+    }
+
 }
