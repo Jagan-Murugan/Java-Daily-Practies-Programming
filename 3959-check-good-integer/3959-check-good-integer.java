93502eaa205d1ee062e995aa5d6c6df2c49199bb
Jagan-Murugan <jagankbd5@gmail.com>
2026-09-12 11:01:32 +0530

    Time: 1 ms (93.57%), Space: 42.2 MB (89.66%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int sum = 0,square = 0;
+        while(n>0){
+            int digit = n%10;
+            sum +=digit;
+            square += digit*digit;
+            n /=10;
+        }
+        if((square - sum) >=50){
+        return true;
+        }
+            return false;
+    }
+}
